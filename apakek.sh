aa962267d139ae61b6cda52c244634be0065d45c
haydark03 <kamilhaydar03@gmail.com>
2024-06-20 21:56:58 +0700

    change name

--- apakek.sh
+++ apakek.sh
@@ -5,17 +5,19 @@ echo '1. haydar kamilhaydar03@gmail.com'
 echo '2. baihaqi baihaqia442@gmail.com'
 sleep 2 
 
-# uninstall for xorg and xorg-server
-# sudo pacman -Rc xorg xorg-server &&
-
 # install gnome dependecies
-sudo pacman -S gnome xorg-xwayland xorg-xlsclients networkmanager gnome-tweaks &&
+sudo pacman -S gnome xorg-xwayland xorg-xlsclients networkmanager gnome-tweaks pipewire &&
 # disable iwd
 sudo systemctl disable iwd.service &&
 sudo systemctl stop iwd.service &&
 # enable NetworkManager
 sudo systemctl enable NetworkManager && 
 sudo systemctl start NetworkManager &&
+# enable bluetooth
+sudo systemctl enable bluetooth &&
+sudo systemctl start bluetooth &&
+# enable pipewire
+systemctl --user enable pipewire &&
 
 #add xwayland gnome-session on tty
 echo 'if [[ -z $DISPLAY && $(tty) == /dev/tty1 && $XDG_SESSION_TYPE == tty ]]; then QT_QPA_PLATFORM=wayland XDG_SESSION_TYPE=wayland exec dbus-run-session gnome-session' >> ~/.bash_profile
